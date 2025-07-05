7e88be8f8139c049efd3a07ca3f685958868c9df
Ananya Singh <ananyasingh1228@gmail.com>
2025-07-05 13:03:30 +0530

    Update Quiz_game.cpp

--- Quiz_game/Quiz_game.cpp
+++ Quiz_game/Quiz_game.cpp
@@ -2,18 +2,19 @@
 #include<fstream>
 using namespace std;
 
+// Structure to store one quiz question and its options
 struct Question {
     string question;
     string optionA;
     string optionB;
     string optionC;
     string optionD;
-    char answer;
+    char answer;// correct option (a/b/c/d)
 };
 
 int main() {
-    Question quiz[10];
-    ifstream F1("QuizSheet.txt");
+    Question quiz[10];// Array to store up to 10 questions
+    ifstream F1("QuizSheet.txt"); // Open the quiz file
 
     int n;
     cout << "Enter the number of questions you want to attempt (5 or 10): ";
@@ -21,7 +22,9 @@ int main() {
     cin.ignore();  //Clears leftover newline after number input
 
     int score = 0;
+     // Loop to read questions from file and ask the user
     for(int i = 0; i < n; i++) {
+        // Read question and options from the file
         getline(F1, quiz[i].question);
         getline(F1, quiz[i].optionA);
         getline(F1, quiz[i].optionB);
@@ -29,12 +32,15 @@ int main() {
         getline(F1, quiz[i].optionD);
         F1 >> quiz[i].answer;
         F1.ignore(); // Clear newline after reading the answer
-
+        
+        // Display the question and options
         cout << "\nQuestion " << (i + 1) << ": " << quiz[i].question << endl;
         cout << quiz[i].optionA << endl;
         cout << quiz[i].optionB << endl;   
         cout << quiz[i].optionC << endl;
         cout << quiz[i].optionD << endl;
+
+        // Take user input
         cout << "Enter your answer (a/b/c/d): ";
         char urAnswer;
         cin >> urAnswer;
